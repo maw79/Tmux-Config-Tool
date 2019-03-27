7ed64327fa8bfa88b86def5b2ceaa2942d7d7e7d
maw79 <arsgate@gmail.com>
2019-03-27 13:21:47 -0500

    Checking for tmux in home directory

--- src/main.py
+++ src/main.py
@@ -1,17 +1,26 @@
 import tkinter as tk
+from tkinter import messagebox
+import os
+from os import path
 # from tkinter import *
 
 window = tk.Tk()
 
 window.title("Tmux Config Tool")
 
-tk.Label(window, text='Bind new horizontal').grid(row=0) 
-tk.Label(window, text='Bind new Vertical').grid(row=1) 
-e1 = tk.Entry(window) 
-e2 = tk.Entry(window) 
-e1.grid(row=0, column=1) 
-e2.grid(row=1, column=1) 
+tk.Label(window, text='Bind new horizontal').grid(row=0)
+tk.Label(window, text='Bind new Vertical').grid(row=1)
+e1 = tk.Entry(window)
+e2 = tk.Entry(window)
+e1.grid(row=0, column=1)
+e2.grid(row=1, column=1)
 
-button = tk.Button(window, text='Apply', width=10, command=window.destroy).grid(row=3) 
+def apply():
+    if(path.exists('~/.tmux.conf')):
+        f = open("~/.tmux.conf", 'w')
+    else:
+        messagebox.showinfo("Error", ".tmux.conf does not exists in your home directory")
+
+button = tk.Button(window, text='Apply', width=10, command=apply).grid(row=3)
 
 window.mainloop()
