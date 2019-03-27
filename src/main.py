a03e43adc13da35b7cdd9bd2ef31e38ab13b2f7c
maw79 <arsgate@gmail.com>
2019-03-26 23:30:27 -0500

    Making the initial window setup for testing

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
-import tkinter as TK
+import tkinter as tk
+# from tkinter import *
 
-window = TK.Tk()
+window = tk.Tk()
+
+window.title("Tmux Config Tool")
+
+tk.Label(window, text='Bind new horizontal').grid(row=0) 
+tk.Label(window, text='Bind new Vertical').grid(row=1) 
+e1 = tk.Entry(window) 
+e2 = tk.Entry(window) 
+e1.grid(row=0, column=1) 
+e2.grid(row=1, column=1) 
+
+button = tk.Button(window, text='Apply', width=10, command=window.destroy).grid(row=3) 
 
 window.mainloop()
